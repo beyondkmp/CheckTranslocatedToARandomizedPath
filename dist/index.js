e7b5b5ac9a97383ba6283d1fbfc537e879b7ea40
payne.fu <payne.fu@ringcentral.com>
2023-03-23 11:35:00 +0800

    change name

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.checkTranslocatedToARandomizedPath = void 0;
+// The native binary will be loaded lazily to avoid any possible crash at start
+// time, which are harder to trace.
+let _nativeModule = undefined;
+function getNativeModule() {
+    _nativeModule = require('bindings')('checkTranslocatedToARandomizedPath.node');
+    return _nativeModule;
+}
+function checkTranslocatedToARandomizedPath() {
+    var _a;
+    const result = (_a = getNativeModule()) === null || _a === void 0 ? void 0 : _a.checkTranslocatedToARandomizedPath();
+    return !!result;
+}
+exports.checkTranslocatedToARandomizedPath = checkTranslocatedToARandomizedPath;
+//# sourceMappingURL=index.js.map
